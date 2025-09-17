3cfef659e34ae8f640147ddf7b13c9d21433d7a2
MEHMET YILDIZ <mehmet.yildizz_@hotmail.com>
2025-09-17 16:54:41 +0300

    ?

---
+++ Assets/Scripts/Managers/GestureManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class GestureManager : MonoBehaviour
+{
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
